c4ad6b082c26300933e772b573b03ebc7691e0f5
Jack Polgar <jack@polgar.id.au>
2014-10-29 03:56:12 +1100

    Allow use of helpers in views without the `raw` filter.

--- src/Helpers/Twig.php
+++ src/Helpers/Twig.php
@@ -15,15 +15,23 @@ class Twig extends Twig_Extension
     public function getFunctions()
     {
         return [
-            new Twig_SimpleFunction('t', ['Radium\Language', 'translate']),
-            new Twig_SimpleFunction('format', ['Traq\Helpers\Format', 'text']),
-            new Twig_SimpleFunction('run_hook', ['Radium\Hook', 'run']),
-            new Twig_SimpleFunction('setting', 'settings'),
+            new Twig_SimpleFunction('t', ['Radium\Language', 'translate'], ['is_safe' => ['html']]),
+            new Twig_SimpleFunction('format', ['Traq\Helpers\Format', 'text'], ['is_safe' => ['html']]),
+            new Twig_SimpleFunction('run_hook', ['Radium\Hook', 'run'], ['is_safe' => ['html']]),
+            new Twig_SimpleFunction('setting', 'settings', ['is_safe' => ['html']]),
             new Twig_SimpleFunction('url', ['Radium\Http\Request', 'basePath']),
 
             // Subscriptions
-            new Twig_SimpleFunction('subscription_link_for', ['Traq\Helpers\Subscription', 'linkFor']),
-            new Twig_SimpleFunction('is_subscribed', ['Traq\Helpers\Subscription', 'isSubscribed'])
+            new Twig_SimpleFunction('subscription_link_for', ['Traq\Helpers\Subscription', 'linkFor'], ['is_safe' => ['html']]),
+            new Twig_SimpleFunction('is_subscribed', ['Traq\Helpers\Subscription', 'isSubscribed']),
+
+            // Render time and memory usage
+            new Twig_SimpleFunction('render_stats', function(){
+                $renderTime  = round((microtime(true) - START_TIME), 2);
+                $memoryUsage = round((memory_get_peak_usage() - START_MEM) / pow(1024, 2), 3);
+
+                return "Rendered in {$renderTime}s using {$memoryUsage}mb of memory";
+            })
         ];
     }
 }
